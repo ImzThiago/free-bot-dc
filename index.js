81cb47ba42f796d00ccddb8621cef891b39ab5a7
ImzThiago <63064963+ImzThiago@users.noreply.github.com>
2020-04-03 02:43:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const config = require('./config.json')
+const bot = new Discord.Client();
+
+
+
+bot.on('ready', message => {
+console.log("Olá Mundo!")
+})
+
+bot.on("message", async message => {
+  
+  let args = message.content.split(" ");
+  let prefix = config.prefix;
+  
+  
+  if(message.content.startsWith(prefix + "botinfo")){
+  message.channel.send({embed:
+  color: 0x00ff00,
+  author: {
+  name: bot.user.username,
+  icon_url: bot.user.avatarURL
+},
+  title: "Informações do Bot",
+  description: `Criador: <@SeuID>\nPrefix: ${prefix}\nUsuarios: ${bot.users.size}\nServidores: ${bot.guilds.size}\nCanais: ${bot.channels.size}`,
+  timestamp: new Date.now();
+  footer: {
+  icon_url: bot.user.avatarURL,
+  text: 'BotInfo'
+}
+})
+
+
+}
+  
+})
+bot.login(config.token)
